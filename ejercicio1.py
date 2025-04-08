c77c75af10e939594d5a8aed1ce56e5dffc2120f
BaZa06 <bazeledon@uamv.edu.ni>
2025-04-08 08:41:55 -0600

    first commit

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+#declarar variables
+nombre_completo =  "José Cristo Carvallo Herrera"
+edad = 17
+altura = 1.72
+soltero= True
+print (f"Nombre completo: {nombre_completo}")
+print (f"Tipo de dato: {type(nombre_completo)}")
+print (f"{edad} {edad}")
+print (f"Tipo de dato: {type(edad)}")
+print (f"{altura:.2f} {altura}")
+print (f"Tipo de dato: {type(altura)}")
+print (f"{soltero} {soltero}")
+print (f"Tipo de dato: {type(soltero)}")
---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+numero1 = 25
+numero5 = 5
+
+print(f"{numero1} > {numero5} el resultado es {numero1 > numero5}")
+print(f"{numero1} < {numero5} el resultado es {numero1 < numero5}")
+print (f"{numero1} >= {numero5} el resultado es {numero1 >= numero5}")
+print(f"{numero1} <= {numero5} el resultado es {numero1 <= numero5}")
+print(f"{numero1} > {numero5} el resultado es {not(numero1 > numero5)}")
+print(f"{numero1} < {numero5} el resultado es {not(numero1 < numero5)}")
+print(f"{numero1} >= {numero5} el resultado es {not(numero1 >= numero5)}")
+print(f"{numero1} <= {numero5} el resultado es {not(numero1 <= numero5)}")
